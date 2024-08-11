e5d9a81c423539dd524d7790c272e9437c4239f4
Patrik Pindeš <patrik.pindes@uniqa.at>
2024-08-11 12:07:46 +0200

    cocktail detail component, back button icon

--- src/app/features/cocktails/pages/cocktail-detail-page/cocktail-detail/cocktail-detail.component.ts
+++ src/app/features/cocktails/pages/cocktail-detail-page/cocktail-detail/cocktail-detail.component.ts
@@ -6,11 +6,12 @@ import {
 } from '@angular/core';
 import { CocktailFavouriteToggleComponent } from '../../../components/cocktail-favourite-toggle/cocktail-favourite-toggle.component';
 import { Cocktail } from '../../../models/cocktail';
+import { AlcoholTagComponent } from '../../../components/alcohol-tag/alcohol-tag.component';
 
 @Component({
   selector: 'app-cocktail-detail',
   standalone: true,
-  imports: [CocktailFavouriteToggleComponent],
+  imports: [CocktailFavouriteToggleComponent, AlcoholTagComponent],
   templateUrl: './cocktail-detail.component.html',
   styleUrl: './cocktail-detail.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
