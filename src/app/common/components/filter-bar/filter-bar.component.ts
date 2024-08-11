af1203de48b1b3a80305917fbb3400fc572b0338
Patrik Pindeš <patrik.pindes@uniqa.at>
2024-08-11 22:58:32 +0200

    filter bar redone in signals

--- src/app/common/components/filter-bar/filter-bar.component.ts
+++ src/app/common/components/filter-bar/filter-bar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-filter-bar',
@@ -10,5 +10,5 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilterBarComponent {
-  @Input() name?: string;
+  name = input<string>();
 }
